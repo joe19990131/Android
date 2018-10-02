93a526ee40f801eede980c71698c32e76fb5a05f
joe19990131 <32476180+joe19990131@users.noreply.github.com>
2018-10-02 16:29:56 +0800

    get bmi

--- BMI/app/src/main/java/com/echoless/bmi/MainActivity.java
+++ BMI/app/src/main/java/com/echoless/bmi/MainActivity.java
@@ -2,12 +2,40 @@ package com.echoless.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+    Scanner sc1 = new Scanner(System.in);
 
+    float weight;
+    float height;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+public void bmi(View view){
+    Log.d("MainActivity","bmi");
+    EditText edWeight = findViewById(R.id.ed_weight);
+    EditText edHeight = findViewById(R.id.ed_height);
+    String w = edWeight.getText().toString();
+    String h = edHeight.getText().toString();
+    float weight = Float.parseFloat(w);
+    float height = Float.parseFloat(h);
+    float bmi = weight/(height*height);
+    Log.d("MainActivity",height+"/"+weight+"/"+bmi);
+
     }
 }
+
+
+
+
+
+
