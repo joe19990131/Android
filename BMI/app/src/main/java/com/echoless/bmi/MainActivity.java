72c4c020f5552a74263e005af970f35c129a5cc6
joe19990131 <32476180+joe19990131@users.noreply.github.com>
2018-10-09 16:23:14 +0800

    bmi初版

--- BMI/app/src/main/java/com/echoless/bmi/MainActivity.java
+++ BMI/app/src/main/java/com/echoless/bmi/MainActivity.java
@@ -1,11 +1,15 @@
 package com.echoless.bmi;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EdgeEffect;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Scanner;
 
@@ -14,24 +18,41 @@ public class MainActivity extends AppCompatActivity {
 
     float weight;
     float height;
+    private EditText edWeight;
+    private EditText edHeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edWeight = findViewById(R.id.ed_weight);
+        edHeight = findViewById(R.id.ed_height);
 
     }
-public void bmi(View view){
-    Log.d("MainActivity","bmi");
-    EditText edWeight = findViewById(R.id.ed_weight);
-    EditText edHeight = findViewById(R.id.ed_height);
-    String w = edWeight.getText().toString();
-    String h = edHeight.getText().toString();
-    float weight = Float.parseFloat(w);
-    float height = Float.parseFloat(h);
-    float bmi = weight/(height*height);
-    Log.d("MainActivity",height+"/"+weight+"/"+bmi);
-
+    public void bmi(View view){
+        //Log.d("MainActivity","bmi");
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight/(height*height);
+        //Log.d("MainActivity",height+"/"+weight+"/"+bmi);
+        //Toast.makeText(this,String.valueOf(bmi),Toast.LENGTH_LONG).show();
+            new AlertDialog.Builder(this)
+                    .setMessage(bmi+"")
+                    .setTitle("console")
+                    .setNegativeButton("CLEAR", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            edHeight.setText("");
+                            edWeight.setText("");
+                        }
+                    })
+                    .setPositiveButton("OK",null)
+                    .show();
     }
+
+
 }
 
 
